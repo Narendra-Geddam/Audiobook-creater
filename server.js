1465110459a209905cebdbe50c80382edc5ff0f3
Narendra-Geddam <don8197257610@gmail.com>
2024-10-21 14:42:35 +0530

    gg

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
+
+// Serve generated audio files
 app.use('/audio', express.static(path.join(__dirname, 'public', 'generated-audios')));
+
+// Serve background music files
+app.use('/bgm', express.static(path.join(__dirname, 'public', 'background-music')));
+
+// Use audio routes
 app.use('/api', audioRoutes);
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
